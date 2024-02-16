1229cc6ed0942be9f96ea2966d655e35f156b38c
johann09 <jonordby@gmail.com>
2024-02-16 03:04:56 +0100

    Initial commit

---
+++ src/main/java/com/example/oblig1kinobiletter/Oblig1KinobiletterApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig1kinobiletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1KinobiletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1KinobiletterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig1kinobiletter/Oblig1KinobiletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1kinobiletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1KinobiletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
